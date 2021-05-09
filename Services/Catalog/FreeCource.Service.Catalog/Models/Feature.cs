3da86af27fe4bf206b09c6699bdf98c61219fecc
Kübra PETEK <kubrapetekk@gmail.com>
2021-05-09 20:07:26 +0300

    Add project files.

---
+++ Services/Catalog/FreeCource.Service.Catalog/Models/Feature.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCource.Service.Catalog.Models
+{
+    public class Feature
+    {
+        public string Duration { get; set; }
+    }
+}
