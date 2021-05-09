62e9e86763f401f6f03d6d69380c6543c0027647
Kübra PETEK <kubrapetekk@gmail.com>
2021-05-09 23:02:44 +0300

    appsetting, mongodb database connection

--- Services/Catalog/FreeCource.Service.Catalog/Startup.cs
+++ Services/Catalog/FreeCource.Service.Catalog/Startup.cs
@@ -1,3 +1,4 @@
+using FreeCource.Service.Catalog.Settings;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -5,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
@@ -27,6 +29,18 @@ namespace FreeCource.Service.Catalog
         {
             services.AddAutoMapper(typeof(Startup));//Bu classa baðlý tüm mapperlarý tarayacak
             services.AddControllers();
+
+
+            //appsettingden database bilgilerini okumak için 
+
+            services.Configure<DatabaseSettings>(Configuration.GetSection("DatabaseSetting"));
+            services.AddSingleton<IDatabaseSettings>(sp =>
+            {
+                return sp.GetRequiredService<IOptions<DatabaseSettings>>().Value;
+            });
+
+
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "FreeCource.Service.Catalog", Version = "v1" });
