98ce7e36e4f8fb5cf8b50a1aea9078707180a2f7
Kübra PETEK <kubrapetekk@gmail.com>
2021-05-15 22:25:59 +0300

    PhotoStock Api projesi oluşturuldu controller ve wwwroot tanımalrı yapıldı

---
+++ Services/PhotoStock/FreeCourse.Services.PhotoStock/Controllers/PhotosController.cs
@@ -0,0 +1,39 @@
+﻿using FreeCourse.Services.PhotoStock.Dtos;
+using FreeCourse.Shared.ControllerBases;
+using FreeCourse.Shared.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.PhotoStock.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhotosController : CustomBaseController
+    {
+        [HttpPost]
+        public async Task<IActionResult> PhotoSave(IFormFile photo, CancellationToken cancellationToken) // CancellationToken cancellationToken --işlem sonlandırılırsa foto kaydetmede sonlandırılsın diye ekledik --otomatik tetiklenir
+        {
+            if (photo!=null && photo.Length>0)
+            {
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photo.FileName);
+                using (var stream=new FileStream(path,FileMode.Create))
+                {
+                    await photo.CopyToAsync(stream, cancellationToken);
+                }
+                //http://www.photostock.api.com/photos/jhasdjashgdj...jpg
+                var returnPath = "photos/" + photo.FileName;
+
+                PhotoDto photoDto = new PhotoDto() { Url = returnPath };
+
+                return CreateActionResultInstance(Response<PhotoDto>.Success(photoDto, 200));
+            }
+            return CreateActionResultInstance(Response<PhotoDto>.Fail("photo is empty", 400));
+        }
+    }
+}
