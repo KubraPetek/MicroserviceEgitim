daf1ce9eb35f9f2344f537d4136776cd815fb6cb
Kübra PETEK <kubrapetekk@gmail.com>
2021-05-21 21:27:32 +0300

    RedisService -redis ile bağlantı kurmayı sağlayan class eklendi , startupa tanımları yapıldı

---
+++ Services/Basket/FreeCourse.Service.Basket/Services/RedisService.cs
@@ -0,0 +1,25 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Service.Basket.Services
+{
+    public class RedisService
+    {
+        private readonly string _host;
+        private readonly int _port;
+
+        private ConnectionMultiplexer _ConnectionMultiplexer;//Package'a eklediğimiz Redis pakektinden geliyor 
+
+        public RedisService(string host, int port)
+        {
+            _host = host;
+            _port = port;
+        }
+
+        public void Connect() => _ConnectionMultiplexer = ConnectionMultiplexer.Connect($"{_host}:{_port}");//Redis ile bağlantı kurulur 
+        public IDatabase GetDb(int db = 1) => _ConnectionMultiplexer.GetDatabase(db);//Database bağlantısını sağlar
+    }
+}
