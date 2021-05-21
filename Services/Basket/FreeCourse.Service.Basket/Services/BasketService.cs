6e4056f06ceac871b57f09ccce7c48207074ba8b
Kübra PETEK <kubrapetekk@gmail.com>
2021-05-21 21:49:02 +0300

    BasketService yazıldı  --Redise get update ve delete metotları yazıldı

---
+++ Services/Basket/FreeCourse.Service.Basket/Services/BasketService.cs
@@ -0,0 +1,40 @@
+﻿using FreeCourse.Service.Basket.Dtos;
+using FreeCourse.Shared.Dtos;
+using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Service.Basket.Services
+{
+    public class BasketService : IBasketService
+    {
+        private readonly RedisService _redisService;
+
+        public BasketService(RedisService redisService)
+        {
+            _redisService = redisService;
+        }
+
+        public async Task<Response<bool>> Delete(string userId)
+        {
+            var status = await _redisService.GetDb().KeyDeleteAsync(userId);
+            return status ? Response<bool>.Success(204) : Response<bool>.Fail("Basket not found", 404);
+        }
+
+        public async Task<Response<BasketDto>> GetBasket(string userId)
+        {
+            var existBasket = await _redisService.GetDb().StringGetAsync(userId);
+            if (String.IsNullOrEmpty(existBasket))
+            {
+                return Response<BasketDto>.Fail("Basket Not Found", 404);
+            }
+            return Response<BasketDto>.Success(JsonSerializer.Deserialize<BasketDto>(existBasket), 200);//JsonSerializer.Deserialize bu yöntemle string olarak gelen değeeri sınıfa çevirebeiliriz 
+        }
+
+        public async Task<Response<bool>> SaveOrUpdate(BasketDto basketDto)
+        {
+            var status = await _redisService.GetDb().StringSetAsync(basketDto.UserId, JsonSerializer.Serialize(basketDto));
+            return status ? Response<bool>.Success(204) : Response<bool>.Fail("Basket could not update or saved", 500);//500 inner server fails 
+        }
+    }
+}
