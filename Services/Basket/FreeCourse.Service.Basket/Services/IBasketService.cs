7a9089c346cc17558f0596ded8d8e6295dd08942
Kübra PETEK <kubrapetekk@gmail.com>
2021-05-21 21:07:08 +0300

    IBasketService eklendi

---
+++ Services/Basket/FreeCourse.Service.Basket/Services/IBasketService.cs
@@ -0,0 +1,17 @@
+﻿using FreeCourse.Service.Basket.Dtos;
+using FreeCourse.Shared.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Service.Basket.Services
+{
+    public interface IBasketService
+    {
+        Task<Response<BasketDto>> GetBasket(string userId);
+        Task<Response<bool>> SaveOrUpdate(BasketDto basketDto);
+        Task<Response<bool>> Delete(string userId);
+
+    }
+}
