a9850c9848500befa469abc5e45e726675fbbae0
Kübra PETEK <kubrapetekk@gmail.com>
2021-05-22 16:09:13 +0300

    DiscountApi ile Identityserver bağlantısı yapıldı

--- IdentityServer/FreeCourse.IdentityServer/Config.cs
+++ IdentityServer/FreeCourse.IdentityServer/Config.cs
@@ -17,6 +17,7 @@ namespace FreeCourse.IdentityServer
             new ApiResource("resorce_catalog"){Scopes={"catalog_fullpermission"}},
             new ApiResource("resource_photo_stock"){Scopes={"photo_stock_fullpermission"}},
             new ApiResource("resource_basket"){Scopes={"basket_fullpermission"}},
+            new ApiResource("resource_discount"){Scopes={"discount_fullpermission"}},
             new ApiResource(IdentityServerConstants.LocalApi.ScopeName)
         };
         public static IEnumerable<IdentityResource> IdentityResources => //burası kullanıcı ile ilgili erişilebilecek  işlemleri tanımlar
@@ -35,6 +36,7 @@ namespace FreeCourse.IdentityServer
                new ApiScope("catalog_fullpermission","Catalog API için full erişim"),
                new ApiScope("photo_stock_fullpermission","Photo Stock API için full erişim"),
                new ApiScope("basket_fullpermission","BasketAPI için full erişim"),
+               new ApiScope("discount_fullpermission","DiscountAPI için full erişim"),
                new ApiScope(IdentityServerConstants.LocalApi.ScopeName)
             };
 
@@ -57,7 +59,7 @@ namespace FreeCourse.IdentityServer
                  AllowOfflineAccess=true,
                  ClientSecrets={new Secret("secret".Sha256())},
                  AllowedGrantTypes=GrantTypes.ResourceOwnerPassword,//refresh token da oluşturur
-                 AllowedScopes={"basket_fullpermission",IdentityServerConstants.StandardScopes.Email, IdentityServerConstants.StandardScopes.OpenId, 
+                 AllowedScopes={"basket_fullpermission","discount_fullpermission",IdentityServerConstants.StandardScopes.Email, IdentityServerConstants.StandardScopes.OpenId, 
                      IdentityServerConstants.StandardScopes.Profile,IdentityServerConstants.LocalApi.ScopeName
                      , IdentityServerConstants.StandardScopes.OfflineAccess },//Offlineaccess refresh token üretir -offline olsa dahi
                  AccessTokenLifetime=1*60*60,//1 saat kullanım belirledik -->token için
--- Services/Discount/FreeCourse.Service.Discount/Startup.cs
+++ Services/Discount/FreeCourse.Service.Discount/Startup.cs
@@ -1,15 +1,14 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IdentityModel.Tokens.Jwt;
+
 
 namespace FreeCourse.Service.Discount
 {
@@ -25,8 +24,19 @@ namespace FreeCourse.Service.Discount
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var requiredAuthorizePolicy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Remove("sub");//sub ý maplerken ismini deðiþtirmesin diye ekledik 
+            //BasketApý'yi IdentityServer ile koruma altýna aldýk
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            {
+                options.Authority = Configuration["IdentityServerURL"];
+                options.Audience = "resource_discount";
+                options.RequireHttpsMetadata = false;
+            });
+            services.AddControllers(opt => {
+                opt.Filters.Add(new AuthorizeFilter(requiredAuthorizePolicy)); //Artýk mutlaka sisteme kayýt olup giriþ yapmýþ kullanýcý bekliyoruz 
 
-            services.AddControllers();
+            });
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "FreeCourse.Service.Discount", Version = "v1" });
@@ -44,7 +54,7 @@ namespace FreeCourse.Service.Discount
             }
 
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
