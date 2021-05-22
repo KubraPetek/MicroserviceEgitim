34ea4be21041697b7a907a382cb48d9ed9cec97d
Kübra PETEK <kubrapetekk@gmail.com>
2021-05-22 13:27:13 +0300

    PostgreSQL içindeki discount tablosuna eşitlenecek  Discount  modeli oluşturuldu

---
+++ Services/Discount/FreeCourse.Service.Discount/Model/Discount.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Service.Discount.Model
+{
+    [Dapper.Contrib.Extensions.Table("discount")] //PostgreSQL içindeki discount tablosu Discount classına eşitlenecek
+    public class Discount
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public int Rate { get; set; }
+        public string Code { get; set; }
+        public DateTime CreatedTime { get; set; }
+    }
+}
