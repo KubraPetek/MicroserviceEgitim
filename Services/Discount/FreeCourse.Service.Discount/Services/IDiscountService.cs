e29a357657fbf75d15d19b399143370c5828a2f0
Kübra PETEK <kubrapetekk@gmail.com>
2021-05-22 13:43:30 +0300

    IDiscountService oluşturuldu

---
+++ Services/Discount/FreeCourse.Service.Discount/Services/IDiscountService.cs
@@ -0,0 +1,23 @@
+﻿using FreeCourse.Shared.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Service.Discount.Services
+{
+    public interface IDiscountService
+    {
+        Task<Response<List<Model.Discount>>> GetAll();
+
+        Task<Response<List<Model.Discount>>> GetById(int id);
+        Task<Response<List<NoContent>>> Save(Model.Discount discount);
+
+        Task<Response<List<NoContent>>> Update(Model.Discount discount);
+
+        Task<Response<List<NoContent>>> Delete(int id);
+
+        Task<Response<List<Model.Discount>>> GetByCodeAndUserId(string code, string userId);
+
+    }
+}
