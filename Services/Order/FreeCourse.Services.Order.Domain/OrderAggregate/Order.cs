bac2616465a0a7728d34ea8386729e8dee623a02
Kübra PETEK <kubrapetekk@gmail.com>
2021-05-28 23:33:00 +0300

    Order,Address, OrderItem eklendi -->DDD yapsına göre

---
+++ Services/Order/FreeCourse.Services.Order.Domain/OrderAggregate/Order.cs
@@ -0,0 +1,42 @@
+﻿using FreeCourse.Services.Order.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FreeCourse.Services.Order.Domain.OrderAggregate
+{
+    //EF Core features  --ORM için DDD kullanım özellikleri
+    //--Owned Types
+    //--Shadow Property
+    //--Backing Fields 
+    public class Order : Entity, IAggregateRoot
+    {
+        public DateTime CreatedDate { get; set; }
+        public Address Address { get; set; }
+        public string BuyerId { get; set; }
+
+        private readonly List<OrderItem> _orderItems;
+
+        public IReadOnlyCollection<OrderItem> OrderItems => _orderItems; //alanı önce private belirleyip kapsülledik , sonrasında sadece okuma olarak dışarıya açmış olduk
+
+        public Order(string buyerId, Address address)
+        {
+            _orderItems = new List<OrderItem>();
+            CreatedDate = DateTime.Now;
+            BuyerId = buyerId;
+            Address = address;
+        }
+
+        public void AddOrderItem(string productId, string productName, decimal price, string pictureUrl)
+        {
+            var existProduct = _orderItems.Any(x => x.ProductId == productId);
+            if (!existProduct)
+            {
+                var newOrderItem = new OrderItem(productId, productName, pictureUrl, price);
+                _orderItems.Add(newOrderItem);
+            }
+        }
+
+        public decimal GetTotalPrice => _orderItems.Sum(x => x.Price);
+    }
+}
