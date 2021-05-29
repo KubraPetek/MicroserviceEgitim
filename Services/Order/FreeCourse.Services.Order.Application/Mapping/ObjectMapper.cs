1f90603948c44d998505ff8591228b37ac538125
Kübra PETEK <kubrapetekk@gmail.com>
2021-05-29 13:56:01 +0300

    AutoMapper konfigürasyonları gerçekleştirildi

---
+++ Services/Order/FreeCourse.Services.Order.Application/Mapping/ObjectMapper.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using System;
+
+namespace FreeCourse.Services.Order.Application.Mapping
+{
+    public static  class ObjectMapper
+    {
+        //Lazy sınıfı obje sadece çağrıldığında initialize edilmesi için kullanılır 
+        private static readonly Lazy<IMapper> lazy = new Lazy<IMapper>(() =>
+          {
+              var config = new MapperConfiguration(cfg =>
+                {
+                    cfg.AddProfile<CustomMapping>();
+                });
+              return config.CreateMapper();
+          });
+
+        public static IMapper Mapper => lazy.Value; //ObjectMapper.Mapper denildiğinde lazy sınıfı initialize edilir 
+    }
+}
