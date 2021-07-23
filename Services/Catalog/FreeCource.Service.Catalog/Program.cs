dd8628e398ef2b078d01d1f4a42294cc14632984
Kübra PETEK <kubrapetekk@gmail.com>
2021-07-23 15:33:51 +0300

    Development için routerlar oluşturuldu , Catalog api ye default katalog datası eklendi

--- Services/Catalog/FreeCource.Service.Catalog/Program.cs
+++ Services/Catalog/FreeCource.Service.Catalog/Program.cs
@@ -1,5 +1,8 @@
+using FreeCource.Service.Catalog.Dtos;
+using FreeCource.Service.Catalog.Services;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -13,7 +16,19 @@ namespace FreeCource.Service.Catalog
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host =CreateHostBuilder(args).Build();
+            using (var scope =host.Services.CreateScope() )
+            {
+                var serviceProvider = scope.ServiceProvider;
+
+                var categoryService = serviceProvider.GetRequiredService<ICategoryService>();
+                if (!categoryService.GetAllAsnyc().Result.Data.Any())
+                {
+                    categoryService.CreateAsync(new CategoryDto { Name = "Asp .Net Kursu" }).Wait();
+                    categoryService.CreateAsync(new CategoryDto { Name = "API Kursu" }).Wait();
+                }
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
