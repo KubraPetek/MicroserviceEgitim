4a4eef93bd6709c74521c9e53c6dc328960a4d5d
Kübra PETEK <kubrapetekk@gmail.com>
2021-07-24 11:49:29 +0300

    Kullanıcıdan veri aldıktan sonra cookie oluşmasına yardım edecek kritik metotlar -IIdentityService ve SigninInput modeli yazdıl

---
+++ Frontends/FreeCourse.Web/Services/Interfaces/IIdentityService.cs
@@ -0,0 +1,20 @@
+﻿using FreeCourse.Shared.Dtos;
+using FreeCourse.Web.Models;
+using IdentityModel.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Web.Services.Interfaces
+{
+    public interface IIdentityService
+    {
+
+        //Kullanıcıdan veri aldıktan sonra cookie oluşmasına yardım edecek kritik metotlar
+        Task<Response<bool>> SignIn(SigninInput signInInput);
+        Task<TokenResponse> GetAccessTokenByRefreshToken();
+        Task RevokeRefreshToken();
+
+    }
+}
