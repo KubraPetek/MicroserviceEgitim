f0dcb34049e42338428e6889e17eab4c1caeaecd
Kübra PETEK <kubrapetekk@gmail.com>
2021-07-24 14:14:57 +0300

    Auth kontrolü işin signin action ve viewleri oluşturuldu

--- Frontends/FreeCourse.Web/Startup.cs
+++ Frontends/FreeCourse.Web/Startup.cs
@@ -1,13 +1,11 @@
 using FreeCourse.Web.Models;
+using FreeCourse.Web.Services;
+using FreeCourse.Web.Services.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace FreeCourse.Web
 {
@@ -23,6 +21,8 @@ namespace FreeCourse.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHttpContextAccessor();
+            services.AddHttpClient<IIdentityService, IdentityService>();
             services.Configure<ClientSettings>(Configuration.GetSection("ClientSettings"));
             services.Configure<ServiceApiSettings>(Configuration.GetSection("ServiceApiSettings"));
 
