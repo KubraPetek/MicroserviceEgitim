7f1d1a3bfa0b5fc220ba8e6757f4de1b2e556ab3
Kübra PETEK <kubrapetekk@gmail.com>
2021-07-26 19:30:01 +0300

    FakePayment mikroservisi için modeller oluşturuldu

--- Services/FakePayment/FreeCourse.Services.FakePayment/Controllers/FakePaymentController.cs
+++ Services/FakePayment/FreeCourse.Services.FakePayment/Controllers/FakePaymentController.cs
@@ -1,4 +1,5 @@
-﻿using FreeCourse.Shared.ControllerBases;
+﻿using FreeCourse.Services.FakePayment.Models;
+using FreeCourse.Shared.ControllerBases;
 using FreeCourse.Shared.Dtos;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,8 +11,9 @@ namespace FreeCourse.Services.FakePayment.Controllers
     public class FakePaymentController : CustomBaseController
     {
         [HttpPost]
-        public IActionResult RecievePayment()
+        public IActionResult RecievePayment(PaymentDto paymentDto )
         {
+            //paymentDto ile ödeme gerçekleştirmek lazım 
             return CreateActionResultInstance<NoContent>(Response<NoContent>.Success(200));
         }
     }
