22399a9058d3a4293f81dd8e87326b0142b156a0
Kübra PETEK <kubrapetekk@gmail.com>
2021-07-26 23:46:23 +0300

    OrderService meteotları yazıldı

--- Frontends/FreeCourse.Web/Services/OrderService.cs
+++ Frontends/FreeCourse.Web/Services/OrderService.cs
@@ -1,22 +1,90 @@
-﻿using FreeCourse.Web.Models.Orders;
+﻿using FreeCourse.Shared.Dtos;
+using FreeCourse.Shared.Services;
+using FreeCourse.Web.Models.FakePayment;
+using FreeCourse.Web.Models.Orders;
 using FreeCourse.Web.Services.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
 
 namespace FreeCourse.Web.Services
 {
     public class OrderService : IOrderService
     {
-        public Task<OrderCreatedViewModel> CreateOrder(CheckOutInfoInput checkOutInfoInput)
+        private readonly IPaymentService _paymentService;
+        private readonly HttpClient _httpClient;
+        private readonly IBasketService _basketService;
+        private readonly ISharedIdentityService _sharedIdentityService;
+
+        public OrderService(IPaymentService paymentService, HttpClient httpClient, IBasketService basketService, ISharedIdentityService sharedIdentityService)
         {
-            throw new NotImplementedException();
+            _paymentService = paymentService;
+            _httpClient = httpClient;
+            _basketService = basketService;
+            _sharedIdentityService = sharedIdentityService;
         }
 
-        public Task<List<OrderViewModel>> GetOrder()
+        public async Task<OrderCreatedViewModel> CreateOrder(CheckOutInfoInput checkOutInfoInput)
         {
-            throw new NotImplementedException();
+            var basket = await _basketService.Get();
+
+            var paymentInfoInput = new PaymentInfoInput()
+            {
+                CardName = checkOutInfoInput.CardName,
+                CardNumber = checkOutInfoInput.CardNumber,
+                Expiration = checkOutInfoInput.Expiration,
+                CVV = checkOutInfoInput.CVV,
+                TotalPrice = checkOutInfoInput.TotalPrice
+            };
+
+            var responsePayment = await _paymentService.ReceivePayment(paymentInfoInput);
+            if (!responsePayment)
+            {
+                return new OrderCreatedViewModel() { Error = "Ödeme alınamadı.", IsSucces = false };
+            }
+
+
+            var orderCreateInput = new OrderCreateInput()
+            {
+                BuyerId = _sharedIdentityService.GetUserId,
+                Address = new AddressCreateInput
+                {
+                    Province = checkOutInfoInput.Province,
+                    District = checkOutInfoInput.District,
+                    Street = checkOutInfoInput.Street,
+                    Line = checkOutInfoInput.Line,
+                    ZipCode = checkOutInfoInput.ZipCode
+                }
+            };
+            basket.BasketItems.ForEach(x =>
+            {
+                var orderItem = new OrderItemCreateInput { ProductId = x.CourseId, PictureUrl = "", Price = x.Price, ProductName = x.CourseName };
+                orderCreateInput.OrderItems.Add(orderItem);
+            });
+
+
+            var response = await _httpClient.PostAsJsonAsync<OrderCreateInput>("orders", orderCreateInput);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return new OrderCreatedViewModel() { Error = "Sipariş oluşturulamadı", IsSucces = false };
+            }
+
+            var orderCreatedViewModel = await response.Content.ReadFromJsonAsync<OrderCreatedViewModel>();
+
+            return orderCreatedViewModel;
+
+        }
+
+        public async Task<List<OrderViewModel>> GetOrder()
+        {
+            var response = await _httpClient.GetFromJsonAsync<Response<List<OrderViewModel>>>("orders");
+
+            return response.Data;
+
         }
 
         public Task SuspendOrder(CheckOutInfoInput checkOutInfoInput)
